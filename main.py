74692d7e9d27336eda9449e9a81c9c69f59bb535
Vako2179 <vakoxm7@gmail.com>
2023-06-09 21:54:59 +0200

    commit for password generator

---
+++ main.py
@@ -0,0 +1,9 @@
+numbers = [3, 8, 12, 5, 17]
+highest_number = numbers[0]  # Assume the first number is the highest
+
+for number in numbers:
+    if number > highest_number:
+        highest_number = number
+
+print(highest_number)
+
